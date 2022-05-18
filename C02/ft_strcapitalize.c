5ffc6a0f63b192367cfa9f7b07f676f353d59020
Isabelle Brito <84083229+bellesbrt@users.noreply.github.com>
2022-05-17 22:05:10 -0300

    Add files via upload

---
+++ C02/ft_strcapitalize.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inicole- <inicole-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/16 03:31:49 by inicole-          #+#    #+#             */
+/*   Updated: 2022/02/17 00:50:41 by inicole-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strlowcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] = str[i] + 32;
+		i++;
+	}
+	return (str);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int	i;
+	int	fg;
+
+	fg = 1;
+	i = 0;
+	ft_strlowcase(str);
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if (fg == 1)
+				str[i] = str[i] - 32;
+			fg = 0;
+		}
+		else if (str[i] >= '0' && str[i] <= 9)
+			fg = 0;
+		else
+			fg = 1;
+		i++;
+	}
+	return (str);
+}
