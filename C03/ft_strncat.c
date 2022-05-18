b74d4069f6c53666ad93b4161e3ebba96d450813
Isabelle Brito <84083229+bellesbrt@users.noreply.github.com>
2022-05-17 22:06:40 -0300

    Add files via upload

---
+++ C03/ft_strncat.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inicole- <inicole-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/17 06:18:44 by inicole-          #+#    #+#             */
+/*   Updated: 2022/02/18 18:04:01 by inicole-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	j = 0;
+	while(dest[i] != '\0')
+		i++;
+	while(src[j] != '\0' && j < nb)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
